3f08fef9a8529db68b3acc8f939ccfccba298943
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-10 18:14:03 +0200

    Functions and scopes

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/*ZADANI */
+// Napište funkci fillcut, která jako svůj první parametr str očekává řetězec a jako druhý parametr len kladné celé číslo. Úkolem funkce je oříznout nebo prodloužit zadaný řetězec tak, aby měl délku přesně len.
+// Pokud je vstupní řetězec delší než len, tak funkce odřízne jeho konec a vrátí výsledek.
+// Pokud je vstupní řetězec kratší než len, tak jej doplní od začátku znakem tečky a vrátí výsledek.
+// Pokud je vstupní řetězec dlouhý přesně len, funkce jej vrátí beze změny.
+
 const fillcut = (str, len) => {
   if (str.length > len) {
     return str.slice(0, len);
